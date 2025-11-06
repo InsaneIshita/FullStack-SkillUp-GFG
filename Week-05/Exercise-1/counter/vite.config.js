83993845542b06f8aee14f2cfda775eddae91fae
Ishita Singh <singhishita3299@gmail.com>
2025-11-06 23:27:23 +0530

    .

--- Week-05/Exercise-1/counter/vite.config.js
+++ Week-05/Exercise-1/counter/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/FullStack-SkillUp-GFG/Week-05/Exercise-1/',
-  plugins: [react()]
+  plugins: [react()],
 })
