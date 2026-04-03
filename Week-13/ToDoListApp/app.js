9476c1a9ef3a8e6c74431b4651149856200884d7
Ishita Singh <singhishita3299@gmail.com>
2026-04-03 18:52:17 +0530

    Update app.js

--- Week-13/ToDoListApp/app.js
+++ Week-13/ToDoListApp/app.js
@@ -13,7 +13,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 const itemsSchema = {
   name: String
